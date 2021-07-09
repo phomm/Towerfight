5603f131c8531468f24422816659c64d09b2c6c4
phomm <phomm@mail.ru>
2021-07-09 19:40:39 +0500

    Added subtraction and multiplication to enemy math formulas

--- models/enemy.py
+++ models/enemy.py
@@ -15,9 +15,31 @@ class Enemy(Actor):
         self.hide()
 
     def calc(self):
-        a1 = randint(0, self._level)
-        a2 = self._level - a1
-        self._math = f"{a1}+{a2}"
+        operation = 0
+        if self._level < 40:
+            operation = randint(0, 1)
+        elif self._level % 30 == 0:
+            operation = 3
+        elif self._level % 20 == 0:
+            operation = 2
+        if self._level <= 20:
+            if self._level % 3 == 0:
+                operation = 3
+            elif self._level % 2 == 0:
+                operation = 2
+            else:
+                operation = randint(0, 1)
+        if operation == 0:
+            a1 = randint(0, self._level)
+            a2 = self._level - a1
+            self._math = f"{a1}+{a2}"
+        elif operation == 1:
+            a1 = randint(0, 10)
+            a2 = self._level + a1
+            self._math = f"{a2}-{a1}"
+        else:
+            a2 = self._level // operation
+            self._math = f"{a2}*{operation}"
 
     def hide(self):
         self._display_level = self._math
