48bf92ea7351df4b0b8ec518baa4538a63433637
phomm <phomm@mail.ru>
2026-03-02 18:48:37 +0100

    Creadits view improved

--- code/gameviewcredits.pas
+++ code/gameviewcredits.pas
@@ -8,14 +8,15 @@ uses Classes,
 type
   TViewCredits = class(TCastleView)
   published
-    ButtonMenu: TCastleButton;
+    ButtonMenu, ButtonCastle, ButtonAuthor: TCastleButton;
   public
     constructor Create(AOwner: TComponent); override;
     procedure Start; override;
-    procedure Update(const SecondsPassed: Single; var HandleInput: boolean); override;
     function Press(const Event: TInputPressRelease): Boolean; override;
   private
     procedure ButtonMenuClick(Sender: TObject);
+    procedure ButtonCastleClick(Sender: TObject);
+    procedure ButtonAuthorClick(Sender: TObject);
   end;
 
 var
@@ -27,7 +28,7 @@ uses
 // System
   SysUtils,
 // Castle  
-  castlewindow;
+  castlewindow, CastleOpenDocument;
 
 constructor TViewCredits.Create(AOwner: TComponent);
 begin
@@ -39,13 +40,8 @@ procedure TViewCredits.Start;
 begin
   inherited;
   ButtonMenu.OnClick := @ButtonMenuClick;
-end;
-
-procedure TViewCredits.Update(const SecondsPassed: Single; var HandleInput: boolean);
-begin
-  inherited;
-  { Executed every frame. }
-
+  ButtonCastle.OnClick := @ButtonCastleClick;
+  ButtonAuthor.OnClick := @ButtonAuthorClick;
 end;
 
 function TViewCredits.Press(const Event: TInputPressRelease): Boolean;
@@ -64,5 +60,15 @@ procedure TViewCredits.ButtonMenuClick(Sender: TObject);
 begin
   Container.PopView();
 end;
+
+procedure TViewCredits.ButtonCastleClick(Sender: TObject);
+begin
+  OpenURL('https://castle-engine.io');
+end;
+
+procedure TViewCredits.ButtonAuthorClick(Sender: TObject);
+begin
+  OpenURL('https://github.com/phomm/Towerfight');
+end;  
 
 end.
